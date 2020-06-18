d671b5a47df96dca71c8ebf8b15a8cae8c60e661
angelrene45 <rene.herrera@lionintel.com>
2020-06-18 09:46:46 -0500

    Modificacion api key y README

--- src/store/actions/pointsActions.js
+++ src/store/actions/pointsActions.js
@@ -1,23 +1,28 @@
 // Middleware redux thunk
 export const createPoint = (newMarket) => {
-    return (dispatch,getState, {getFirebase,getFirestore}) => {
+    return async (dispatch,getState, {getFirebase,getFirestore}) => {
 
         // hacer una llamada asincrona a fireStore
         const firestore = getFirestore();
-        firestore.collection('points').add({
-            ...newMarket,
-            authorFirstName:"Rene",
-            authorLastName:'Herrera',
-            authorId:1546545
-        }).then(() => {
-            // Despachamos
+
+        try {
+            const resultFireStore = await firestore.collection('points').add({
+                ...newMarket,
+                authorFirstName: "Rene",
+                authorLastName: 'Herrera',
+                authorId: 1546545
+            });
+
+            // Despachamos en pointsReducer
             dispatch({
                 type:'CREATE_POINT',
                 payload: newMarket
-            })
-        }).catch((err) => {
-            console.log(err);
-        });
+            });
+
+        } catch (e) {
+            // Error Firebase
+            console.log(e)
+        }
 
     }
 };
